055a04c755018af5436975e0e4daf3cda557a9bf
Lara-Mamedio <lmamedios@gmail.com>
2020-10-06 16:45:33 -0300

    viz todos os exercicios e não fiz o desafio

---
+++ semana4/planner/aula10-dom/aula10-dom/index.js
@@ -0,0 +1,14 @@
+function imprimeNome() {
+    const meuInput = document.getElementById("meu-input")
+    const meuInput2 = document.getElementById("meu-input2")
+    console.log(meuInput.value)
+    console.log(meuInput2.value)
+}
+
+function passouPorCima() {
+    console.log("Passou por cima, sai daqui!")
+}
+
+function saiuDeCima() {
+    console.log("Ufa, tava precisando de um ar!")
+}
