9804c23887dbc32463e2d03d6e3992977912263b
Lara-Mamedio <lmamedios@gmail.com>
2020-10-09 16:39:51 -0300

    exercício 1 de interpretação de código esta resulvido

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+//1. interpretação de código
+//legenda: nesse exercício o que será impresso no console, estara após o este sinal => (igual seguido de maior) e terminara no . (ponto).
+
+    //exercício 1
+    //resposta:
+        /*Você insere o atual valor do dolar na const "cotacao".
+        No return é acresentado o R$ e a "cotacao" e o "valorEmDolar" são multiplicados. 
+        Na const "meuDinheiro" se chama a função pra executala e o valor que você quer converter para reais você coloca em "valorEmDolar". 
+        No console.log é impresso o valor em reais com o R$ junto, ou seja, é uma função para converter dinheiro de dolar para reais => R$552,98.*/
